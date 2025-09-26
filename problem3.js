8426f517c76a7589bc00173fa997225effac6029
Mamun123123123 <almalm90459@gmail.com>
2025-09-26 21:35:19 +0600

    divisible by x

--- problem3.js
+++ problem3.js
@@ -1,5 +1,5 @@
 // count of numbers divisible by x 
-let n =20,x=3
+let n =100,x=5
 let num = [...Array(n).keys()].map(x=>x+1).filter(item => item % x === 0) // show
 let result = Math.floor(n/x) // ocunt
 console.log((num));
