ad7e69a43190e420f2546b019fe9cf00cac7f9c0
Mamun123123123 <almalm90459@gmail.com>
2025-09-26 22:04:59 +0600

    check all problem

--- problem2.js
+++ problem2.js
@@ -2,4 +2,4 @@
 let arr = [2,4,1,5,10],i=1,j=3
 let result = arr.map((s=>x=>s+=x)(0)) // 12,2+4=6, aivabe sum koreeee.......
 let result1 = result[j] - (i>0?result[i-1]:0) // subarray sum
-console.log(result1);
+console.log(result);
--- problem5.js
+++ problem5.js
@@ -1,7 +1,7 @@
 let arr = [1,2,2,3,4,5,6,2,7,3]
-let freq={}
-arr.forEach(x => {
-    freq[x] = (freq[x] || 0) + 1
-});
-
+let freq = {}
+arr.map(item => {
+    freq[item] = (freq[item] || 0) + 1
+})
 console.log(freq);
+
