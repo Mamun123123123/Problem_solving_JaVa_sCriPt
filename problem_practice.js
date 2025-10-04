184355fcccd2bcc13bf068b289876c2b9be963b2
Mamun123123123 <almalm90459@gmail.com>
2025-10-04 11:08:57 +0600

    problem

--- problem_practice.js
+++ problem_practice.js
@@ -6,21 +6,54 @@
 // let resul = [...new Set(ar)]
 // console.log(resul);
 
-let ar = ["apple","banana","apple","orange"]
-let freq = {}
-for (const food of ar) {
-    freq[food] = (freq[food]|| 0 ) + 1
-}   
-let s = []
-for (const item in freq) {
-    if(freq[item] === 1)
-        s.push(item)
+// let ar = ["apple","banana","apple","orange"]
+// let freq = {}
+// for (const food of ar) {
+//     freq[food] = (freq[food]|| 0 ) + 1
+// }   
+// let s = []
+// for (const item in freq) {
+//     if(freq[item] === 1)
+//         s.push(item)
         
-}
-let resul = s.toString().split("").filter(item => item.match(/[aeiouAEIOU]/g))
-console.log(resul);
+// }
+// let resul = s.toString().split("").filter(item => item.match(/[aeiouAEIOU]/g))
+// console.log(resul);
 
+// let arr = [10,20,30,40,50]
+// let resul = arr.reduce((sum,item)=>sum+item,0)
+// console.log(resul);
 
-
-
+// let str = "levelef"
+// let pstr = str.split("").reverse().join("")
+// console.log(pstr);
+// if(pstr === str)
+// {
+//     console.log("palindrom");
     
+// }else{
+//     console.log("not palindrom");
+    
+// }
+
+// let str = "javascript is fun"
+// let result = str.split(" ").map(item=>item.charAt(0).toUpperCase() + item.slice(1)).join(" ")
+// console.log(result);
+
+// let student = {
+//     name:"Mamun",marks:{
+//         math:80,eng:70,sci:90
+//     }
+// }
+// let resul = Object.values(student.marks).reduce((a,b)=>a+b,0)
+
+// console.log(resul);
+
+
+let ob =[
+    {name:"A",age:20},{name:"B",age:25},{name:"C",age:22}
+
+]
+  
+let maxAge = Math.max(...ob.map(item => item.age));
+console.log("Max Age:", maxAge);
